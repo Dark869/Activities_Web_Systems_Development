db018127a3427db806a1a068f73248b72060c10d
Dark869 <ing.victor97@gmail.com>
2024-10-27 00:57:49 -0600

    Cambios minimos

---
+++ Form/indexl.js
@@ -0,0 +1,19 @@
+(function (){
+
+
+window.onload = function() {
+    let form = document.getElementById('form');
+    form.onsubmit = function(event) {
+        let errores = new Array();
+
+        let name = document.getElementById('name');
+        let age = document.getElementById('edad');
+        let email = document.getElementById('email');
+        let password = document.getElementById('password');
+
+        
+
+    };
+};
+
+})();
